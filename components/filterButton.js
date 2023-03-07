01570d45035b8ba50848e6848be5b9fcb872364e
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-03-06 23:14:03 -0500

    added ordering button to alphebetize and order by time created

--- components/filterButton.js
+++ components/filterButton.js
@@ -2,9 +2,8 @@ import renderToDOM from '../utils/renderToDom';
 
 const filterButton = () => {
   const domString = `<div id="filtering">
-  <label for="filter-value">Filter by Category</label>
+  <label for="filter-value">Filter by:</label>
   <select class="form-control" id="filter-value" required>
-    <option value="">Category</option>
     <option value="all">All</option>
     <option value="animal">Animal</option>
     <option value="weird">Weird</option>
@@ -12,6 +11,18 @@ const filterButton = () => {
   </select>
   <span id="filter-btn-div">
   <button type="button" class="btn btn-primary" id="filter-btn">Filter!</button>
+  </span>
+</div>
+<div id="ordering">
+  <label for="order-value">Order by:</label>
+  <select class="form-control" id="order-value" required>
+    <option value=""></option>
+    <option value="A-Z">A-Z</option>
+    <option value="Newest">Newest</option>
+    <option value="Oldest">Oldest</option>
+  </select>
+  <span id="filter-btn-div">
+  <button type="button" class="btn btn-primary" id="order-btn">Order!</button>
   </span>
 </div>
 `;
